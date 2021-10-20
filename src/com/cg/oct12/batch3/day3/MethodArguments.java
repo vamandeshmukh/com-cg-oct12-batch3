60392c375cf61a8ce77129ccdcb93d3961f66f45
vamandeshmukh <vamandeshmukh@gmail.com>
2021-10-20 17:22:18 +0530

    Update MethodArguments.java

--- src/com/cg/oct12/batch3/day3/MethodArguments.java
+++ src/com/cg/oct12/batch3/day3/MethodArguments.java
@@ -2,4 +2,20 @@ package com.cg.oct12.batch3.day3;
 
 public class MethodArguments {
 
+	static int addTwoNumbers(int i, int j) {
+		return i + j;
+	}
+
+	static void addTwoAndPrint(int i, int j) {
+		System.out.println(i + j);
+	}
+
+	public static void main(String[] args) {
+
+		int output = MethodArguments.addTwoNumbers(10, 20);
+
+		System.out.println(output);
+		System.out.println(MethodArguments.addTwoNumbers(20, 30));
+		MethodArguments.addTwoAndPrint(10, 20);
+	}
 }
