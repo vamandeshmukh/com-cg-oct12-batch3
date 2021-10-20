3e1d2436d702db8a843c55062b092d67f6373c09
vamandeshmukh <vamandeshmukh@gmail.com>
2021-10-20 14:17:53 +0530

    C

--- src/com/cg/oct12/batch3/day3/App3.java
+++ src/com/cg/oct12/batch3/day3/App3.java
@@ -1,5 +1,53 @@
 package com.cg.oct12.batch3.day3;
 
+// Entities 
+
+// Employee - characteristics 
+// eid, name, salary, doj, email, addrress 
+
+//Employee - behaviours 
+// work, applyForleave, writeCode, testCode 
+
+// OOP - Object Oriented Programming 
+
+class Employee {
+
+	// fields - characteristics
+	int eid;
+	String name;
+	double salary;
+
+	// methods - behaviours
+	void work() {
+		System.out.println("Employee works...");
+	}
+}
+
+class BankCustomer {
+
+	static String ifsc; // free access
+	int accountNumber;
+	String customerName;
+	double balance;
+
+	static void deposit() {
+
+	}
+
+	void withdraw() {
+
+	}
+
+	void transfer() {
+
+	}
+
+	static void openAccount() {
+
+	}
+
+}
+
 public class App3 {
 
 	public static void main(String[] args) {
---
+++ src/com/cg/oct12/batch3/day3/MethodDemo.java
@@ -0,0 +1,17 @@
+package com.cg.oct12.batch3.day3;
+
+public class MethodDemo {
+
+	static void hello() {
+		System.out.println("Hello world!");
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("Start");
+		hello();
+		System.out.println("End");
+
+	}
+
+}
