ce9e0c7838c28d0d3ce0a10597434efa703db486
vamandeshmukh <vamandeshmukh@gmail.com>
2021-10-21 12:25:59 +0530

    Update FinalDemo.java

--- src/com/cg/oct12/batch3/day4/FinalDemo.java
+++ src/com/cg/oct12/batch3/day4/FinalDemo.java
@@ -1,11 +1,24 @@
 package com.cg.oct12.batch3.day4;
 
+/*
+ * final keyword - specifier 
+ * final field / variable - value can not be changed 
+ * final method - can not be overridden 
+ * final class - can not be extended 
+ */
+
 public class FinalDemo {
+
+//	final static int numberOfOvers = 20;
+	final static int NUMBER_OF_OVERS = 20;
 
 	private static final int num = 10;
 	private static int num2 = 20;
 
 	public static void main(String[] args) {
+
+		final int num3 = 30;
+//		num3 = 35; // CE 
 
 		System.out.println(FinalDemo.num);
 		System.out.println(FinalDemo.num2);
