1375760617b06c5fc9a45af07cdb5ee202b12f60
vamandeshmukh <vamandeshmukh@gmail.com>
2021-10-21 12:38:45 +0530

    Create JavaPackageDemo.java

---
+++ src/com/cg/oct12/batch3/day4/JavaPackageDemo.java
@@ -0,0 +1,28 @@
+package com.cg.oct12.batch3.day4;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class JavaPackageDemo {
+
+	public static void main(String[] args) {
+
+//		System.out.println(Integer.SIZE);
+//		System.out.println(Integer.MIN_VALUE);
+//		System.out.println(Integer.MAX_VALUE);
+
+//		int num = 2147483647;
+//		int num2 = 2147483648;
+
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Please enter your name:");
+//
+//		String name = sc.next();
+//		System.out.println("Welcome " + name);
+
+		Random random = new Random();
+		System.out.println(random.nextInt());
+
+	}
+
+}
