f9341af5a81a4fc623e1ac4079aa3bab7897f24f
vamandeshmukh <vamandeshmukh@gmail.com>
2021-10-21 10:01:21 +0530

    Update ConstructorDemo.java

--- src/com/cg/oct12/batch3/day4/ConstructorDemo.java
+++ src/com/cg/oct12/batch3/day4/ConstructorDemo.java
@@ -19,9 +19,19 @@ class Employee {
 	String name;
 	double salary;
 
-	Employee() {
-		System.out.println("Employee constructor");
+	public Employee() {
+		System.out.println("zero-args constructor");
+
 	}
+
+	public Employee(int eid, String name, double salary) {
+		super();
+		this.eid = eid;
+		this.name = name;
+		this.salary = salary;
+		System.out.println("parameterized constructor");
+	}
+
 }
 
 public class ConstructorDemo {
@@ -39,5 +49,8 @@ public class ConstructorDemo {
 		emp2.name = "Ajeet";
 		emp2.salary = 10.6;
 		System.out.println(emp2.eid + " " + emp2.name + " " + emp2.salary);
+
+		Employee emp3 = new Employee(103, "Sonu", 12.75);
+		System.out.println(emp3.eid + " " + emp3.name + " " + emp3.salary);
 	}
 }
