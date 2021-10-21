eb53b514209d0b7ed290f6fcb9d9b571fb166ec1
vamandeshmukh <vamandeshmukh@gmail.com>
2021-10-21 10:37:30 +0530

    Update ThisDemo.java

--- src/com/cg/oct12/batch3/day4/ThisDemo.java
+++ src/com/cg/oct12/batch3/day4/ThisDemo.java
@@ -11,21 +11,22 @@ public class ThisDemo {
 	int num2;
 
 	static void staticMethod() {
-		System.out.println(ThisDemo.num1);
+		System.out.println("staticMethod " + ThisDemo.num1);
 		ThisDemo obj = new ThisDemo();
-		System.out.println(obj.num2);
+		System.out.println("staticMethod " + obj.num2);
 	}
 
 	void nonStaticMethod() {
-		System.out.println(ThisDemo.num1);
-		System.out.println(this.num2);
+		System.out.println("nonStaticMethod " + ThisDemo.num1);
+		System.out.println("nonStaticMethod " + this.num2);
 	}
 
 	public static void main(String[] args) {
 
 		ThisDemo.staticMethod();
-		ThisDemo obj = new ThisDemo();
-		obj.nonStaticMethod();
+		ThisDemo obj2 = new ThisDemo();
+		obj2.num2 = 10;
+		obj2.nonStaticMethod();
 
 	}
 
