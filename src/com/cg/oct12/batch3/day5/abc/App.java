4dfa567ffb44368200e8ac55b891dd8407532533
vamandeshmukh <vamandeshmukh@gmail.com>
2021-10-22 12:18:52 +0530

    Update App.java

--- src/com/cg/oct12/batch3/day5/abc/App.java
+++ src/com/cg/oct12/batch3/day5/abc/App.java
@@ -4,22 +4,60 @@ package com.cg.oct12.batch3.day5.abc;
 
 // interface 
 
+// abstract method - 
+// has the keyword abstract 
+// has only method signature 
+// does not have method body 
+// can be defined in abstract class 
+// can not be defined in concrete class 
+
+// abstract class 
+// has the keyword abstract 
+// object can not be created 
+// can contain zero or more abstract methods 
+// can contain zero or more concrete methods 
+// can also contain both 
+
+interface FinanceMinistry {
+
+	public abstract void payInterestOnDeposits();
+
+}
+
+interface StateGovt {
+
+	public abstract void paySalaryToEmployees();
+
+}
+
 abstract class Rbi {
 
-	abstract void doKyc(); // Aadhaar, PAN, Passport, ...
+	public abstract void doKyc(); // Aadhaar, PAN, Passport, ...
 
 	void openAccount() {
+		System.out.println("Open account");
 	}
 
 	void payLoans() {
+		System.out.println("pay loans");
 	}
 }
 
-class HdfcBank extends Rbi {
+class HdfcBank extends Rbi implements FinanceMinistry, StateGovt {
 
 	@Override
-	void doKyc() {
+	public void doKyc() {
 		System.out.println("Aadhaar");
+	}
+
+	@Override
+	public void paySalaryToEmployees() {
+		System.out.println("Saalry");
+	}
+
+	@Override
+	public void payInterestOnDeposits() {
+		System.out.println("Interest");
 	}
 
 }
@@ -27,11 +65,10 @@ class HdfcBank extends Rbi {
 class IciciBank extends Rbi {
 
 	@Override
-	void doKyc() {
+	public void doKyc() {
 		System.out.println("PAN Card");
 	}
 }
-
 
 public class App {
 
@@ -39,8 +76,12 @@ public class App {
 
 		HdfcBank obj = new HdfcBank();
 		obj.doKyc();
+		obj.openAccount();
+		obj.payLoans();
 		IciciBank obj2 = new IciciBank();
 		obj2.doKyc();
+
+//		Rbi obj3 = new Rbi();
 	}
 
 }
