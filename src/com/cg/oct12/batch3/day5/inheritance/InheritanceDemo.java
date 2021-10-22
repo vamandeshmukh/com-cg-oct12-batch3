f893555fd7f9cfbc892d551612f3d82c9c825f86
vamandeshmukh <vamandeshmukh@gmail.com>
2021-10-22 10:35:25 +0530

    Create InheritanceDemo.java

---
+++ src/com/cg/oct12/batch3/day5/inheritance/InheritanceDemo.java
@@ -0,0 +1,27 @@
+package com.cg.oct12.batch3.day5.inheritance;
+
+class Parent {
+
+	int num;
+
+}
+
+class Child extends Parent {
+
+	int num2;
+
+}
+
+public class InheritanceDemo {
+
+	public static void main(String[] args) {
+
+		Child obj = new Child();
+		obj.num2 = 20;
+		System.out.println(obj.num2);
+
+		obj.num = 10;
+		System.out.println(obj.num);
+	}
+
+}
