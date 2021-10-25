e129cf6f87256fa7c76d1ca883001bf364b4c81b
vamandeshmukh <vamandeshmukh@gmail.com>
2021-10-25 14:55:55 +0530

    Update ColDemo.java

--- src/com/cg/oct12/batch3/day7/col/ColDemo.java
+++ src/com/cg/oct12/batch3/day7/col/ColDemo.java
@@ -8,22 +8,31 @@ public class ColDemo {
 
 	public static void main(String[] args) {
 
-		int[] arr = new int[5];
-		System.out.println(arr[4]);
+//		int[] arr = new int[5];
+//		System.out.println(arr[4]);
 
 		ArrayList myList = new ArrayList();
-
-		App obj = new App();
-
+		System.out.println(myList);
+		System.out.println(myList.size());
 		myList.add(10);
 		myList.add(10.5);
 		myList.add("abc");
-		myList.add(new App());
-		myList.add(obj);
-
 		System.out.println(myList);
+		System.out.println(myList.size());
+		myList.add("aaa");
+		System.out.println(myList);
+		System.out.println(myList.size());
+		myList.remove(2);
+		System.out.println(myList);
+		System.out.println(myList.size());
 
-		System.out.println(myList.get(2));
+		ArrayList myList2 = new ArrayList();
+		myList2.add(22);
+//		myList2.add(myList); // comment out the next line 
+		myList2.addAll(myList); // comment out the previous line 
+		myList2.add("www");
+		System.out.println(myList2);
+		System.out.println(myList2.size());
 
 	}
 }
