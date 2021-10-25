f085d9e809533cb8d6f7f03085bf5580ee276253
vamandeshmukh <vamandeshmukh@gmail.com>
2021-10-25 11:12:19 +0530

    Update RegexDemo2.java

--- src/com/cg/oct12/batch3/day7/RegexDemo2.java
+++ src/com/cg/oct12/batch3/day7/RegexDemo2.java
@@ -7,7 +7,11 @@ public class RegexDemo2 {
 
 	public static void main(String[] args) {
 
-		String regex = "[a-z]", input = "P"; // abc xyz ijk // ABC 123
+//		String regex = "[a-zA-Z0-9#@\\h]{5}", input = "a_  @"; // abc xyz ijk // ABC 123
+//		String regex = "[\\d]{3}", input = "457"; // [0-9]{3}
+		String regex = "[\\D]{3}", input = "awE"; // [non digit characters]{3}
+//		String regex = "[\\w]{3}", input = "4Ya"; // [a-zA-Z0-9]{3}
+//		String regex = "[\\w]{3}", input = "%Ya"; // abc xyz ijk // ABC 123
 
 		Pattern pattern = Pattern.compile(regex);
 
