076bf5311f94b00d1385c8c112516dce9b2d5b6c
vamandeshmukh <vamandeshmukh@gmail.com>
2021-10-27 14:50:05 +0530

    Update Employee.java

--- src/com/cg/oct12/batch3/day8/equals/Employee.java
+++ src/com/cg/oct12/batch3/day8/equals/Employee.java
@@ -7,7 +7,7 @@ public class Employee {
 	double salary;
 
 	public Employee() {
-
+		super();
 	}
 
 	public Employee(int eid, String name, double salary) {
@@ -16,10 +16,43 @@ public class Employee {
 		this.name = name;
 		this.salary = salary;
 	}
-	
-	
-	
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + eid;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(salary);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Employee)) {
+			return false;
+		}
+		Employee other = (Employee) obj;
+		if (eid != other.eid) {
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		if (Double.doubleToLongBits(salary) != Double.doubleToLongBits(other.salary)) {
+			return false;
+		}
+		return true;
+	}
 
 	@Override
 	public String toString() {
